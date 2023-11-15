e72e1235115ce4fdbac17b765ddea78f6d38053d
Nikita <shovet619@gmail.com>
2023-11-15 20:34:02 +0700

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -63,7 +63,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
-          content="Независимое SOCIAL & digital агентство"
+          content="HIT THE MARKET | МАРКЕТИНГОВОЕ АГЕНСТВО"
         />
       </Head>
 
